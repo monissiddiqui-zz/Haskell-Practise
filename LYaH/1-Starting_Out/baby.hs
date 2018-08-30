8e0b6faf82d9cf2d5b983b0161c7a1acb789d959
Monis Siddiqui <monisqsiddiqui@gmail.com>
2018-08-29 23:01:30 -0400

    Initial commit

---
+++ LYaH/1-Starting_Out/baby.hs
@@ -0,0 +1,12 @@
+doubleMe x = x + x
+doubleUs x y = doubleMe x + doubleMe y
+
+doubleSmallNumber x = if x <100 then x else x*2
+doubleSmallNumber' x = (if x <100 then x else x*2) +1
+
+boomBangs xs = [if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
+
+nouns = ["hobo","frog","pope"]
+adjectives = ["lazy","grouchy","scheming"]
+
+removeNoneUppercase st = [c | c <- st, c `elem` ['A'..'Z']]
